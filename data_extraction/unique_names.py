7c40e1294d4b9b02af3bd4f6e7d80bd7b6e58a92
Marko Prelevikj <pr3mar@users.noreply.github.com>
2020-11-06 12:03:03 +0100

    Repo reorganization

---
+++ data_extraction/unique_names.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import csv
+
+file_names = [
+    'imena_deckov', 'imena_deklic', 'moska_imena', 'zenska_imena'
+]
+
+for file_name in file_names:
+    names = pd.read_csv(f'../data/{file_name}_all.csv')
+    df = pd.DataFrame(names["FIRST_NAME"].unique(), columns=["FIRST_NAME"])
+    df.to_csv(f'../data/{file_name}.csv', index=False, quoting=csv.QUOTE_ALL)
+
---
+++ train/crosloeng.py
@@ -0,0 +1,2 @@
+if __name__ == '__main__':
+    print("Here go the CroSloEng model specifics")
