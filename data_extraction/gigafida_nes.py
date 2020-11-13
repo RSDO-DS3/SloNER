585b42f7c58cf3f3290991e76e044d5d5242488e
Marko Prelevikj <pr3mar@users.noreply.github.com>
2020-11-13 15:39:36 +0100

    Gigafida extracting NEs to csv

--- data_extraction/gigafida_nes.py
+++ data_extraction/gigafida_nes.py
@@ -2,8 +2,9 @@ import os
 import shutil
 import subprocess
 import pandas as pd
+import time
 
-from xml.etree import cElementTree
+from bs4 import BeautifulSoup
 
 
 def list_dir(dirpath: str) -> (list, list):
@@ -12,7 +13,7 @@ def list_dir(dirpath: str) -> (list, list):
         files.extend(fnames)
         dirs.extend(dnames)
         break
-    return dirs, files
+    return dirs, sorted(files)
 
 
 def extract_data(fname: str, dir: str) -> int:
@@ -33,22 +34,68 @@ def delete_dir(dname: str) -> bool:
 
 
 def extract_nes(dname: str) -> pd.DataFrame:
+    print(f"Extracting NEs from {dname}")
+    nes = []
+    with open(dname) as f:
+        data = BeautifulSoup(f, 'lxml')
+        segs = data.find_all("seg", {"type": "name"})
+        for seg in segs:
+            for i, w in enumerate(seg.find_all("w")):
+                nes.append({
+                    "word": w.getText(),
+                    "lemma": w["lemma"],
+                    "msd": w["msd"],
+                    "type": f'{"B" if i == 0 else "I"}-{seg["subtype"]}'
+                })
+    return pd.DataFrame(nes)
+
+
+def process_gigafida_chunk(in_dir: str, out_dir: str) -> pd.DataFrame:
+    _, files = list_dir(in_dir)
+    chunk_data = pd.DataFrame()
+    for file in files:
+        file_nes = extract_nes(f"{in_dir}/{file}")
+        file_nes.to_csv(f"{out_dir}/{file.split('.')[0]}.csv", index=False)
+        chunk_data = pd.concat([chunk_data, file_nes], ignore_index=True)
+    chunk_data.to_csv(f"{out_dir}.csv", index=False)
     return pd.DataFrame()
 
 
-def process_gigafida_chunk(chunk: str) -> pd.DataFrame:
-    return pd.DataFrame()
-
-
-def extract_gigafida_nes(dir: str):
-    dnames, fnames = list_dir(dir)
+def extract_gigafida_nes(in_dir: str, out_dir: str):
+    dnames, fnames = list_dir(in_dir)
+    start_time = time.time()
     for fname in fnames:
+        chunk_time = time.time()
         chunk_name = fname.split(".")[0]
-        print(f"Chunk name: {chunk_name}")
-        # extract_data(fname=f"{gigafida_dir}/{fname}", dir=gigafida_dir)
+        chunk_dir = f"{in_dir}/{chunk_name}"
+        print(f"Processing chunk: {chunk_name}")
+
+        # extract if not already extracted
+        extract_time = time.time()
+        if not (os.path.exists(chunk_dir) and os.path.isdir(chunk_dir)):
+            extract_data(fname=f"{gigafida_dir}/{fname}", dir=gigafida_dir)
+        print(f"Finished extracting in: {time.time() - extract_time:.3f}")
+
+        # create chunk output directory
+        chunk_out_dir = f"{out_dir}/{chunk_name}"
+        if not os.path.exists(chunk_out_dir):
+            os.mkdir(chunk_out_dir)
+
+        # process the chunk
+        process_time = time.time()
+        process_gigafida_chunk(f"{in_dir}/{chunk_name}", chunk_out_dir)
+        print(f"Finished processing in: {time.time() - process_time:.3f}")
+
+        # delete the extracted files
+        del_time = time.time()
+        delete_dir(chunk_dir)
+        print(f"Finished deleting in: {time.time() - del_time:.3f}")
+        print(f"Finished processing chunk {chunk_name} in: {time.time() - chunk_time:.3f}")
+    print(f"Finished all processing in: {time.time() - start_time:.3f}")
 
 
 if __name__ == '__main__':
     print("Extracting Gigafida's NEs")
     gigafida_dir = './data/datasets/gigafida2.1'  # relative to workdir
-    extract_gigafida_nes(gigafida_dir)
+    gigafida_out_dir = './data/ne/gigafida/'
+    extract_gigafida_nes(gigafida_dir, gigafida_out_dir)
